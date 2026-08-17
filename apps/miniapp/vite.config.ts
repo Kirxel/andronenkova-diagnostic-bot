96e6a34ab8ea723554a402668f2a6b4b134eba7d
Кирилл Элерт <elertk133@gmail.com>
2026-08-17 12:27:47 +0700

    add allowedhosts

--- apps/miniapp/vite.config.ts
+++ apps/miniapp/vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    allowedHosts: ["andronenkova.ru", "www.andronenkova.ru"]
+  },
   plugins: [react()],
   test: {
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts"
   }
 });
-
